641392040b4eb9e491b54a7d4c5255c556496226
tduncan14 <nbatre1@yahoo.com>
2024-11-15 06:18:54 -0600

    keep moving forward

---
+++ dashboard/src/app/(dashboard)/layout.tsx
@@ -0,0 +1,35 @@
+
+import Link from "next/link";
+import Image from "next/image";
+import Menu from "@/components/Menu";
+
+export default function DashboardLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+
+
+        <div className="h-screen flex">
+            {/* left */}
+            <div className="left  w-1/6  md:w[8%] lg:w-[16%]  xl:w-[14%] bg-red-50 p-4" >
+                <Link className="flex items-center justify-center gap-2 lg:justify-start" href="/" >
+                    <Image src="/logo.png" alt="logo" width={32} height={32} />
+                    <span className="hidden lg:block"> Evanstontownship</span>
+                </Link>
+
+
+                <Menu />
+            </div>
+
+
+            {/* right */}
+            <div className="right w-5/6 md:[92%] lg:w-[84%] xl:w-[86%] bg-purple-300">
+                r
+            </div>
+        </div>
+
+
+    );
+}
---
+++ dashboard/src/app/(dashboard)/student/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const studentPage = () => {
+    return (
+        <div>
+            <h1> hello student</h1>
+
+        </div>
+    )
+}
+
+export default studentPage
